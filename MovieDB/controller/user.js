d95f83a2ac9bb66768c287d86059cfce4b46ce33
amandagao2015 <amandagao2015@gmail.com>
2020-12-15 23:19:34 -0800

    Update

--- MovieDB/controller/user.js
+++ MovieDB/controller/user.js
@@ -5,25 +5,74 @@ const Campground = require("../schema/movieSchema");
 const middleware = require("../middleware");
 
 
+var localUser = "";
 // User profile
-router.get("/users/:id", middleware.isLoggedIn, (req, res) => {
+router.get("/profile", middleware.isLoggedIn, (req, res) => {
+    Campground.find().where("author.id").equals(req.user).exec((err, movies) => {
+        if (err) {
+            req.flash("error", "Something went wrong...");
+            res.redirect("/movies");
+        } else { res.render("profile",{user: req.user, movies: movies})}
+    });
+
+});
+
+
+// show edit form
+router.get("/profile/edit", middleware.isLoggedIn, (req, res) => {
+    this.localUser = req.user;
+    res.render("editProfile", { user: req.user});
+});
+
+
+// update profile
+router.put("/profile/edit", middleware.isLoggedIn, (req, res) => {
+    console.log(req.body)
+    User.findByIdAndUpdate(this.localUser._id, req.body.user, (err, updatedUser) => {
+        res.redirect("/profile");
+    });
+});
+
+
+
+// User profile
+router.get("/profile/:id", (req, res) => {
     User.findById(req.params.id, (err, foundUser) => {
         if (err || !foundUser) {
             req.flash("error", "Something went wrong...");
             res.redirect("/movies");
-        } else {
-            Campground.find().where("author.id").equals(foundUser._id).exec((err, movies) => {
-                if (err) {
-                    req.flash("error", "Something went wrong...");
-                    res.redirect("/movies");
-                } else { res.render("profile",{user: foundUser, movies: movies})}
-            });
+        }
+        else{
+            if (!req.user) {
+                Campground.find().where("author.id").equals(foundUser._id).exec((err, movies) => {
+                    if (err) {
+                        req.flash("error", "Something went wrong...");
+                        res.redirect("/movies");
+                    } else { res.render("profileForOthers",{user: foundUser, movies: movies})}
+                });
+            }
+            else if (req.params.id !== req.user._id) {
+                Campground.find().where("author.id").equals(foundUser._id).exec((err, movies) => {
+                    if (err) {
+                        req.flash("error", "Something went wrong...");
+                        res.redirect("/movies");
+                    } else { res.render("profileForOthers",{user: foundUser, movies: movies})}
+                });
+            }
+            else {
+                Campground.find().where("author.id").equals(foundUser._id).exec((err, movies) => {
+                    if (err) {
+                        req.flash("error", "Something went wrong...");
+                        res.redirect("/movies");
+                    } else { res.render("profile",{user: foundUser, movies: movies})}
+                });
+            }
         }
     });
 });
 
 // show edit form
-router.get("/users/:id/edit", middleware.isLoggedIn, (req, res) => {
+router.get("/profile/:id/edit", (req, res) => {
     User.findById(req.params.id, (err, foundUser) => {
         if (err || !foundUser) { return res.redirect("back"); }
         if (foundUser._id.equals(req.user._id)) {
@@ -35,8 +84,9 @@ router.get("/users/:id/edit", middleware.isLoggedIn, (req, res) => {
     });
 });
 
+
 // update profile
-router.put("/users/:id", middleware.isLoggedIn, (req, res) => {
+router.put("/profile/:id", (req, res) => {
     User.findByIdAndUpdate(req.params.id, req.body.user, (err, updatedUser) => {
         if (err) {
             if (err.name === 'MongoError' && err.code === 11000) {
